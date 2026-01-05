7cba66023e3b0ce2cad0f14fc1f3f422dd27f3fa
sivasainathreddyg <sainathreddy1909@gmail.com>
2026-01-05 18:30:29 +0530

    create the required folders

---
+++ template.sh
@@ -0,0 +1,20 @@
+# Creating the Directory
+
+mkdir -p validator
+
+#Creating the Files
+
+touch validator/__init__.py
+touch validator/BannedWordsList.py
+touch validator/CucumberExpressionMatch.py
+touch validator/SecretsPresent.py
+touch validator/HasValidURL.py
+touch validator/CompetitorCheck.py
+touch validator/BiasCheck.py
+touch validator/DetectPII.py
+touch validator/ProfanityFree.py
+touch app.py
+
+echo "Directory and files created sucessfully"
+
+
